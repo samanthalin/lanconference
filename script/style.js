82e93147bb5a4454c6b52e6bf3e6546ba251da39
samantha lin <samanthalin1992@gmail.com>
2020-03-17 03:48:21 +0800

    shopping cart

--- script/style.js
+++ script/style.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-	
+	//shopping cart
+	//--q1
+	$('#shoppingCart .q1 .option').click(function(){
+		$(this).siblings().removeClass('selected');
+		$(this).toggleClass('selected');
+	});
+	//--q3
+	$('#shoppingCart .q3 .option').click(function(){
+		$(this).toggleClass('selected');
+	});
 	//footer
 	var currentYear = (new Date).getFullYear();
 	$(".current-year").text( (new Date).getFullYear() );
