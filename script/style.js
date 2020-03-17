11eda67c23497d268c4ebdb035fb1a9d5abf2ae3
samantha lin <samanthalin1992@gmail.com>
2020-03-18 02:10:24 +0800

    inquery message

--- script/style.js
+++ script/style.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+	//inquery
+	$('#inquery form .submit').click(function(){
+		$('#inquery form').hide();
+		$('#inquery .message').show()
+		window.scrollTo(0, 0);
+	});
+
 	//shopping cart
 	//--q1
 	$('#shoppingCart .q1 .option').click(function(){
